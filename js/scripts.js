2d59cb28a3a408e21d4cf6339e5ef398aacd3a61
Josh Bertsche and Jacob Hefley <jacob@>
2016-03-22 13:37:04 -0700

    add spans to test jquery effects

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
-$(document).ready(function() {
-  $(".clickable").click(function() {
-    $("#walrus-showing").toggle();
-    $("#walrus-hidden").toggle();
+$(function() {
+  $(".slide-up").click(function() {
+    $("img").slideUp();
+  });
+  $(".slide-down").click(function() {
+    $("img").slideDown();
+  });
+  $(".slide-toggle").click(function() {
+    $("img").slideToggle();
+  });
+  $(".fade-in").click(function() {
+    $("img").fadeIn();
+  });
+  $(".fade-out").click(function() {
+    $("img").fadeOut();
+  });
+  $(".fade-toggle").click(function() {
+    $("img").fadeToggle();
   });
 });
